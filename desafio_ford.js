b126d8388f17b61c052d725631bf87a2303d594e
JRGCast <joao.ralph1@gmail.com>
2021-10-04 11:26:50 -0300

    adicionada regra do exemplo dado

--- desafio_ford.js
+++ desafio_ford.js
@@ -1,6 +1,16 @@
-
+// [“Rio Branco”,”Macapá”], [“Macapá”,”Manaus”], [“Belém”,”Porto Velho”], [“Boa
+//   Vista”,”Palmas”], [“Belém”,”Rio Branco”], [“Palmas”,”Rio Branco”], [“Boa
+//   Vista”,”Salvador”], [“Maceió”,”Fortaleza”], [“São Luís”,”Salvador”], [“João
+//   Pessoa”,”Recife”], [“Recife”,”Teresina”], [“Teresina”,”Natal”], [“Aracaju”,”Salvador”],
+//   [“Natal”,”Aracaju”], [“Goiânia”,”Cuiabá”], [“Cuiabá”,”Campo Grande”], [“Campo
+//   Grande”,”Brasília”], [“Vitória”, “Cuiabá”], [“Vitória”,”Belo Horizonte”], [“Belo
+//   Horizonte”,”São Paulo”], [“São Paulo”,”Rio de Janeiro”], [“Rio de Janeiro”,”Curitiba”],
+//   [“Curitiba”,”Florianópolis”], [“Florianópolis”,”Porto Alegre”]
 const rotas = (partida = 'Brasília', entrega = 'Brasília') => {
-  const totalRotas = 0;
+  // Norte já tem 1 rota direto para o Nordeste;
+  // Sudeste já tem uma rota para o centro-oeste e para o sul;
+  // totas as capitais da mesma região já se conectam (que é o que já tem nas rotas unidirecionais, bem como no exemplo dado);
+  let totalRotas = 0;
   const capitaisNorte = ['Belém', 'Boa Vista', 'Macapá', 'Manaus', 'Palmas', 'Porto Velho', 'Rio Branco'];
   const capitaisNordeste = ['Aracaju', 'Fortaleza', 'João Pessoa', 'Natal', 'Maceió', 'Recife', 'Salvador', 'São Luís', 'Teresina'];
   const capitaisCentro = ['Brasília', 'Campo Grande', 'Cuiabá', 'Goiânia'];
@@ -8,7 +18,12 @@ const rotas = (partida = 'Brasília', entrega = 'Brasília') => {
   const capitaisSul = ['Curitiba', 'Florienópolis', 'Porto Alegre'];
   const todasMenosCentro = capitaisNorte.concat(capitaisNordeste, capitaisSudeste, capitaisSul).sort();
 
-  if (partida === entrega) return `Adicionando ${totalRotas} rotas.`;
+  if (partida === entrega) totalRotas + 0;
+  if ((capitaisNorte.find(cidade => cidade === partida)) && capitaisNordeste.find(cidade => cidade === entrega)) totalRotas + 0;
+  if (((capitaisSudeste.find(cidade => cidade === partida)) && capitaisCentro.find(cidade => cidade === entrega))) totalRotas + 0;
+  if ((capitaisSudeste.find(cidade => cidade === partida)) && capitaisSul.find(cidade => cidade === entrega)) totalRotas + 0;
+
+  return `Adicionando ${totalRotas} rotas`;
 };
 
-console.log(rotas('Brasília', 'Brasília'));
+console.log(rotas('Belém', 'Aracaju'));
